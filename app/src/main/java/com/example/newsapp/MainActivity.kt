56bd10f76add8d8c74f15d0ad534218e8f38a678
Adil Ansari <ansari89561@gmail.com>
2024-12-06 14:53:50 +0530

    Initial commit add only splash screen

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        installSplashScreen()
+        setContent {
+            NewsAppTheme {
+
+            }
+        }
+    }
+}
+
